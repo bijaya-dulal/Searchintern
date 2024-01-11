8bab7aae45006cf0b7cbae0a0b62676f6c048510
bijaya-dulal <dulalbijaya0@gmail.com>
2024-01-11 21:28:17 +0545

    tailwind setup

--- searchintern/settings.py
+++ searchintern/settings.py
@@ -33,14 +33,23 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    # 'django_browser_reload.middleware.BrowserReloadMiddleware',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user_details',
     'student_register',
+    'Tailwindcss',
+    'tailwind',
+    'django_browser_reload'
+
     
 ]
+TAILWIND_APP_NAME = 'Tailwindcss'
+
+INTERNAL_IPS = ["127.0.0.1",
+                ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -118,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'Tailwindcss/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- searchintern/urls.py
+++ searchintern/urls.py
@@ -15,15 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from searchintern import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('_reload_/',include('django_browser_reload.urls')),
     path('',views.homepage, name='homepage'),
     path('homepage/',views.homepage, name='homepage'),
     path('login/',views.login, name='login'),
     path('about_us/',views.aboutUs, name='aboutus'),
     path('register/',views.register, name ="register"),
     path('userprofile',views.userprofile, name ="userprofile"),
+
 ]
